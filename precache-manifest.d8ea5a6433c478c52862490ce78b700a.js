452d296c9a3de4672a8cab35fdf7a88c09411cc7
shrutika <sm77shruti@gmail.com>
2020-09-18 11:32:22 +0530

    Updates

---
+++ precache-manifest.d8ea5a6433c478c52862490ce78b700a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8126e4183222c32bd6b1a193670386fa",
+    "url": "/GITHUB-profile-reactjs/index.html"
+  },
+  {
+    "revision": "1fb052ab6c95f1e098cb",
+    "url": "/GITHUB-profile-reactjs/static/css/main.1075486b.chunk.css"
+  },
+  {
+    "revision": "3fbbd008c8896406c836",
+    "url": "/GITHUB-profile-reactjs/static/js/2.20660fbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GITHUB-profile-reactjs/static/js/2.20660fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb052ab6c95f1e098cb",
+    "url": "/GITHUB-profile-reactjs/static/js/main.98b13be7.chunk.js"
+  },
+  {
+    "revision": "936d00bccd2859130216",
+    "url": "/GITHUB-profile-reactjs/static/js/runtime-main.0cdf9ec5.js"
+  }
+]);
